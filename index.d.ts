2d48c9f73cb1a138e69078d5337815dcea2233c4
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-07-14 18:01:59 +0300

    No global declaration :/

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,5 @@ interface ArtifactsGlobal {
     require <A> (name: string): truffle.TruffleContract<A>
 }
 
-global {
-    declare function contract (name: string, callback: (accounts: Array<string>) => void): void
-    declare var artifacts: ArtifactsGlobal
-}
+declare function contract (name: string, callback: (accounts: Array<string>) => void): void
+declare var artifacts: ArtifactsGlobal
