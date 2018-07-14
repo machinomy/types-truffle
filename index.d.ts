df8ad41b64c7f965452d1d09389f72c5d00aa8d7
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-07-14 16:27:50 +0300

    Properly declare global variables and functions

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,8 @@
-declare module 'truffle' {
-    import * as truffle from 'truffle-contract'
+import * as truffle from 'truffle-contract'
 
-    interface ArtifactsGlobal {
-        require <A> (name: string): truffle.TruffleContract<A>
-    }
-
-    global {
-        function contract (name: string, callback: (accounts: Array<string>) => void): void
-        const artifacts: ArtifactsGlobal
-    }
+interface ArtifactsGlobal {
+    require <A> (name: string): truffle.TruffleContract<A>
 }
+
+declare function contract (name: string, callback: (accounts: Array<string>) => void): void
+declare var artifacts: ArtifactsGlobal
